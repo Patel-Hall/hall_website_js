9b3946139f560f327dd98d59102b710aa9bd1d6d
Nikhil <nikhilrajani309@gmail.com>
2024-06-28 15:58:04 +0530

    Fixed logout error

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -8,12 +8,22 @@ const page = () => {
 
   if(session?.user===null) redirect('/admin/login');
 
+  const logoutUser = async()=>{
+    try {
+        await signOut();
+
+        redirect("/admin/login");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
   return (
     <div className="grid place-items-center">
       <h1>Logged into Patel Hall of Residence Admin Panel</h1>
       <h2>Name: {session?.user?.name}</h2>
       <h2>Email: {session?.user?.email}</h2>
-      <button onClick={()=>signOut()} className='bg-primary px-6 py-2 mt-2'>Log out</button>
+      <button onClick={logoutUser} className='bg-primary px-6 py-2 mt-2'>Log out</button>
     </div>
   )
 }
